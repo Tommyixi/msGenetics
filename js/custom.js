f9a9b6bbab8a615a74a150e28c960cd44ad5c407
Tommyixi <carpenito.t@gmail.com>
2016-01-22 16:11:23 -0800

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,70 @@
+$(document)
+  .ready(function() {
+
+    // fix menu when passed
+    $('.masthead')
+      .visibility({
+        once: false,
+        onBottomPassed: function() {
+          $('.fixed.menu').transition('fade in');
+        },
+        onBottomPassedReverse: function() {
+          $('.fixed.menu').transition('fade out');
+        }
+      })
+    ;
+
+    $(function () {
+      $('.unravel').textillate();
+    })
+
+    // create sidebar and attach to menu open
+    $('.ui.sidebar')
+      .sidebar('attach events', '.toc.item')
+    ;
+
+    $(".show-enrollment-case").click(function(e){
+      e.preventDefault();
+      $('.ui.modal.case-control').modal('show');    
+    })
+
+    $(".show-enrollment-multi").click(function(e){
+      e.preventDefault();
+      $('.ui.modal.case-multi').modal('show');    
+    })    
+
+    $(".show-enrollment-african").click(function(e){
+      e.preventDefault();
+      $('.ui.modal.case-african').modal('show');    
+    })   
+
+    $(".show-enrollment-genes").click(function(e){
+      e.preventDefault();
+      $('.ui.modal.case-genes').modal('show');    
+    })   
+
+    $(".show-enrollment-pediatric").click(function(e){
+      e.preventDefault();
+      $('.ui.modal.case-pediatric').modal('show');    
+    })   
+
+    $(".show-enrollment-twins").click(function(e){
+      e.preventDefault();
+      $('.ui.modal.case-twins').modal('show');    
+    })        
+
+    $(".show-enrollment-microbiome").click(function(e){
+      e.preventDefault();
+      $('.ui.modal.case-microbiome').modal('show');    
+    })     
+
+    $(".remove").click(function(e){
+      e.preventDefault();
+      $(".modal").modal('hide');
+    })             
+
+    $(".join-study").click(function(){
+      window.open('https://redcap.ucsf.edu/surveys/?s=RPKGc4');
+    })
+
+  });
