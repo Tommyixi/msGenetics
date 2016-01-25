e1fb649ee2fe13f52245b9c7c72f8ef6abaf266a
Tommyixi <carpenito.t@gmail.com>
2016-01-25 08:58:31 -0800

    Added scroll on research tab, removed unneccesary style sheet, fixed links

--- js/custom.js
+++ js/custom.js
@@ -67,4 +67,11 @@ $(document)
       window.open('https://redcap.ucsf.edu/surveys/?s=RPKGc4');
     })
 
+    $('.research-link').click(function(e){
+      e.preventDefault()
+      $('html, body').animate({
+        scrollTop: $("#" + this.classList[1]).offset().top
+        },1000 );
+    })
+
   });
