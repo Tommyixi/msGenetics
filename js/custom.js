dc1f38906207b4a5f96ec7cbb15f32b0a415fc45
Tommyixi <carpenito.t@gmail.com>
2016-07-07 12:15:05 -0700

    Allows for following menu

--- js/custom.js
+++ js/custom.js
@@ -5,4 +5,23 @@ $(document).ready(function() {
   $(".join-study").click(function(){
     window.open('https://redcap.ucsf.edu/surveys/?s=849FYFLPA7');
   })
+  
+	// fix menu when passed
+	$('.masthead')
+	  .visibility({
+	    once: false,
+	    onBottomPassed: function() {
+	      $('.fixed.menu').transition('fade in');
+	    },
+	    onBottomPassedReverse: function() {
+	      $('.fixed.menu').transition('fade out');
+	    }
+	  })
+	;
+
+	// create sidebar and attach to menu open
+	$('.ui.sidebar')
+	  .sidebar('attach events', '.toc.item')
+	;
 });
+
